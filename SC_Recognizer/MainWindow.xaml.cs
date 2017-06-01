2efbd77dc58d733ca80e40969b455387d04cacf2
Hollow Memories <Hollow Memories>
2017-06-01 22:24:10 +0300

    Image zoom was added

--- SC_Recognizer/MainWindow.xaml.cs
+++ SC_Recognizer/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
 
 namespace SC_Recognizer
 {
@@ -20,9 +22,69 @@ namespace SC_Recognizer
     /// </summary>
     public partial class MainWindow : Window
     {
+        double scale = 1.0;
+        double minScale = 0.5;
+        double maxScale = 5.0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Image documents (.jpg)|*.jpg";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                //SourceImage.Source = filename;
+                
+                BitmapImage bi3 = new BitmapImage();
+                bi3.BeginInit();
+                bi3.UriSource = new Uri(filename, UriKind.Relative);
+                bi3.CacheOption = BitmapCacheOption.OnLoad;
+                bi3.EndInit();
+                //SourceImage.Stretch = Stretch.Fill;
+                SourceImage.Source = bi3;
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void SourceImage_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var position = e.MouseDevice.GetPosition(SourceImage);
+
+            if (e.Delta > 0)
+                scale += 0.1;
+            else
+                scale -= 0.1;
+
+            if (scale > maxScale)
+                scale = maxScale;
+            if (scale < minScale)
+                scale = minScale;
+
+            SourceImage.RenderTransform = new ScaleTransform(scale, scale);
         }
     }
 }
