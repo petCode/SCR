2604c098c4463e27760f58a6db2b057ae06798e9
Hollow Memories <Hollow Memories>
2017-06-02 02:51:54 +0300

    SCR_OpenCV project was added

--- SC_Recognizer/MainWindow.xaml.cs
+++ SC_Recognizer/MainWindow.xaml.cs
@@ -33,29 +33,22 @@ namespace SC_Recognizer
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            // Create OpenFileDialog 
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
 
-            // Set filter for file extension and default file extension 
-            dlg.DefaultExt = ".txt";
+            dlg.DefaultExt = ".jpg";
             dlg.Filter = "Image documents (.jpg)|*.jpg";
 
-            // Display OpenFileDialog by calling ShowDialog method 
             Nullable<bool> result = dlg.ShowDialog();
 
-            // Get the selected file name and display in a TextBox 
             if (result == true)
             {
-                // Open document 
                 string filename = dlg.FileName;
-                //SourceImage.Source = filename;
                 
                 BitmapImage bi3 = new BitmapImage();
                 bi3.BeginInit();
                 bi3.UriSource = new Uri(filename, UriKind.Relative);
                 bi3.CacheOption = BitmapCacheOption.OnLoad;
                 bi3.EndInit();
-                //SourceImage.Stretch = Stretch.Fill;
                 SourceImage.Source = bi3;
             }
         }
